8b15394578267b9c3cae690d74f3d7957618f39f
Roman Yerin <r.yerin@640kb.co.uk>
2024-11-11 11:15:59 +0100

    Remove certificates out of the codebase

--- interfaces/low_level.go
+++ interfaces/low_level.go
@@ -16,6 +16,8 @@ const NID_SNILS int = (int)(C.NID_SNILS)
 const NID_CN int = (int)(C.NID_commonName)
 const NID_givenName int = (int)(C.NID_givenName)
 const NID_surname int = (int)(C.NID_surname)
+const NID_OGRN = (int)(C.NID_OGRN)
+const NID_OGRNIP = (int)(C.NID_OGRNIP)
 
 func VerifyPkcs7(ca []byte, signature []byte, data []byte) bool {
 	return C.verify_pkcs7(C.CBytes(ca), C.size_t(len(ca)), C.CBytes(signature), C.size_t(len(signature)), C.CBytes(data), C.size_t(len(data))) == 1
